35c99e32cb4daa8d289ab34178dec7070d55f056
gelNye <nigelxhudson@gmail.com>
2022-01-21 22:17:59 -0500

    RayCast

---
+++ Assets/Script/RayCasting.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCasting : MonoBehaviour {
+
+    public Camera gameCamera;
+    public LayerMask mask;
+
+    // Update is called once per frame
+    void Update() {
+
+        Ray ray = gameCamera.ScreenPointToRay(Input.mousePoition);
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast (ray, out hitInfo, 100, mask, QueryTriggerInteraction.Ignore)) {
+            Destroy(hitInfo.collider.gameObject);
+
+                    
+            
+
+        }
+    }
+}
