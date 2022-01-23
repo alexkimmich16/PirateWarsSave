abae101ba4ad23e70de3ed000947d9e0426b603b
gelNye <nigelxhudson@gmail.com>
2022-01-23 15:59:54 -0500

    RayCast 2

--- Assets/Script/RayCasting.cs
+++ Assets/Script/RayCasting.cs
@@ -5,18 +5,16 @@ using UnityEngine;
 public class RayCasting : MonoBehaviour {
 
     public Camera gameCamera;
-    public LayerMask mask;
+    public gameObject.tag.Interactable
 
     // Update is called once per frame
     void Update() {
 
-        Ray ray = gameCamera.ScreenPointToRay(Input.mousePoition);
+        Ray ray = gameCamera.ScreenPointToRay(Input.mousePosition);
         RaycastHit hitInfo;
 
-        if (Physics.Raycast (ray, out hitInfo, 100, mask, QueryTriggerInteraction.Ignore)) {
-            Destroy(hitInfo.collider.gameObject);
-
-                    
+        if (Physics.Raycast (ray, out hitInfo, 100, gameObject.tag.Interactable, QueryTriggerInteraction.Ignore)) {
+            Destroy(hitInfo.transform.gameObject);
             
 
         }
