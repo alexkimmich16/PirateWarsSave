f350bcd3cc04834aff58a751eeaa7bbc74aeec0c
alexkimmich16 <alexkimmich16@gmail.com>
2022-02-14 20:56:28 -0500

    Deleted SHitty VFX scripts

--- Assets/VFX/Extra/Script/ExtraPropertiesForEditor.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ExtraPropertiesForEditor : MonoBehaviour
-{
-    [Tooltip("When pressed Preview Effect, what is the starting spread.")]
-    public float defaultSpread = 1;
-}
