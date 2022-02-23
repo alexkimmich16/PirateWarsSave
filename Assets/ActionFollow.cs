2113c7ce10b705b48a168fd526dc6a20177b8bbe
alexkimmich16 <alexkimmich16@gmail.com>
2022-02-23 14:22:59 -0500

    laptop commit

---
+++ Assets/ActionFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionFollow : MonoBehaviour
+{
+    public Vector2 Max;
+    public Vector2 Min;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
